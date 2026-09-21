945fc442f5ad52d617c6799864f4d6b3ed1732d2
Vutlhari23 <vutlhari23m@gamil.com>
2026-09-21 11:01:17 +0200

    Attach a middle ware to ther express app

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
 import express ,{type Express} from 'express';
-
+import bodyParser from 'body-parser'
 //create an express application
 const app :Express = express();
 const PORT = process.env.PORT || 3000;
+
+//Attach the express app to the application\
+app.use(express.json);
+app.use(bodyParser.json)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Listen to the incoming requests from the server.
 app.listen(PORT, ()=> {
