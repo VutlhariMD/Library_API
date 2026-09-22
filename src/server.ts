e5f7773d88ea14ab2f43af4de22dc7596bd60663
Vutlhari23 <vutlhari23m@gamil.com>
2026-09-22 23:13:58 +0200

    Core :server setup

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
-import express ,{type Express} from 'express';
-import bodyParser from 'body-parser'
-import { loggerMiddleware } from './middleware/logger.js';
-//create an express application
-const app :Express = express();
+import express, {Express}  from 'express';
+
+const app  : Express = express();
+
 const PORT = process.env.PORT || 3000;
 
-//Attach the express app to the application\
-app.use(express.json);
-app.use(bodyParser.json)
+app.use((req, res)=>{
 
+    res.status(200).json({message : "The server is running."})
+});
 
-app.use(loggerMiddleware);
+app.listen(PORT , 
+    ()=>{ console.log (`Server is running on http://localhost:${PORT}`)}
+);
 
-
-
-//Listen to the incoming requests from the server.
-app.listen(PORT, ()=> {
-    console.log(`Server is running on http://localhost: ${PORT}`);
-})
+    
